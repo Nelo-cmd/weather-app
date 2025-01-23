6d08f75d846f50f31e59cb31ad5023364c1d702f
Nelo-cmd <nelliana170@gmail.com>
2025-01-23 10:41:47 +0100

    working on using fetch, and throwing error message

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { error } from "ajv/dist/vocabularies/applicator/dependencies";
 import React, { useState, useEffect } from "react";
 
 function App() {
@@ -7,7 +8,24 @@ function App() {
   useEffect(() => {
     const fetchWeatherData = () => {
       console.log("fetching Data...");
+      if (!SubmittedCity) {
+        throw new error("Please input a city");
+        return;
+      }
+      const API_KEY = "b74242ac8d01b6711e1f6bb0628cf56e";
+      const url = `https://api.openweathermap.org/data/2.5/forecast?q=${SubmittedCity}&appid=${API_KEY}`;
+      try {
+        const response = fetch(url);
+        if (!response.ok) {
+          return Error("city not found");
+        }
+        const data = response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("error fetching weather data:", error.message);
+      }
     };
+    fetchWeatherData();
   }, [SubmittedCity]);
 
   return (
